0aceb2a92673e0af2f67142c0d3a31dea2907bf1
sobhan <sobhanolyayi@gmail.com>
2024-06-26 19:43:02 +0330

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,74 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      
+      <div>
+        <div>
+          <div>
+            <img></img>
+            <span></span>
+          </div>
+          <div>
+            <span>خانه</span>
+            <span>آموزش ها</span>
+            <span>اخبار و تخفیف ها</span>
+            <span>اینستاگرام ما</span>
+            <span>تماس با ما</span>
+          </div>
+          <div>
+            <button></button>
+            <button></button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <img></img>
+            </div>
+            <div>
+              <h3>فروشگاه خرید گیفت کارت</h3>
+              <br></br>
+              <h5>اعتماد شما اعتبار ماست</h5>
+            </div>
+          </div>
+          <div>
+            <img></img>
+            <span></span>
+            <br></br>
+            <span></span>
+            <br></br>
+            <span></span>
+            <br></br>
+            <span></span>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <img></img>
+          <button></button>
+        </div>
+        <div>
+          <img></img>
+          <button></button>
+        </div>
+        <div>
+          <img></img>
+          <button></button>
+        </div>
+        <div>
+          <img></img>
+          <button></button>
+        </div>
+        <div>
+          <img></img>
+          <button></button>
+        </div>
+        <div>
+          <img></img>
+          <button></button>
+        </div>
+      </div>
     </main>
   );
 }
