3efe27c9e204db160570c66a177b541ff64cd332
sobhan <sobhanolyayi@gmail.com>
2024-06-27 02:09:19 +0330

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,22 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <div>
-        <div>
-          <div>
-            <img></img>
-            <span></span>
+      <div className={styles.headerdiv}>
+        <div className={styles.headerupperdiv}>
+          <div className={styles.headerlogodiv}>
+            <Image src={"/rocket.png"} alt={"icon"} width={45} height={45}></Image>
+            <span className={styles.headernamespan}>فروش گیفت کارت</span>
           </div>
-          <div>
-            <span>خانه</span>
-            <span>آموزش ها</span>
-            <span>اخبار و تخفیف ها</span>
-            <span>اینستاگرام ما</span>
-            <span>تماس با ما</span>
+          <div className={styles.headermenudiv}>
+            <span className={styles.headermenuspan}>خانه</span>
+            <span className={styles.headermenuspan}>آموزش ها</span>
+            <span className={styles.headermenuspan}>اخبار و تخفیف ها</span>
+            <span className={styles.headermenuspan}>اینستاگرام ما</span>
+            <span className={styles.headermenuspan}>تماس با ما</span>
           </div>
-          <div>
-            <button></button>
-            <button></button>
+          <div className={styles.headersignindiv}>
+            <button className={styles.headersigninbutton}>ثبت نام</button>
+            <button className={styles.headersigninbutton}>ورود</button>
           </div>
         </div>
         <div>
