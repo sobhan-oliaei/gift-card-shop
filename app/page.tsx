952ea0ab7393d819a7ca2ac0c1c4f55e646d65fe
sobhan <sobhanolyayi@gmail.com>
2024-07-05 00:18:39 +0330

    a

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         </div>
         <div className={styles.headerlowerdiv}>
           <div className={styles.headerlowerrightdiv}>
-              <Image className={styles.headerimg} src={"/giftcard.png"} alt={"icon"} width={350} height={200}></Image>
+            <Image className={styles.headerimg} src={"/giftcard.png"} alt={"icon"} width={350} height={200}></Image>
             <div className={styles.headernamediv}>
               <h1>فروشگاه خرید گیفت کارت</h1>
               <br></br>
@@ -68,7 +68,7 @@ export default function Home() {
           <button className={styles.productbutton}>ایکس باکس(xbox)</button>
         </div>
         <div className={styles.productdiv}>
-          <Image className={styles.productimg} src={"/playstation.png"} alt={"icon"} width={140} height={120} style={{marginTop: "17px", marginBottom:"10px"}}></Image>
+          <Image className={styles.productimg} src={"/playstation.png"} alt={"icon"} width={140} height={120} style={{ marginTop: "17px", marginBottom: "10px" }}></Image>
           <button className={styles.productbutton}>پلی استیشن</button>
         </div>
         <div className={styles.productline}></div>
@@ -77,11 +77,11 @@ export default function Home() {
           <button className={styles.productbutton}>اسپاتیفای (spotify)</button>
         </div>
         <div className={styles.productdiv}>
-          <Image className={styles.productimg} src={"/mastercard.png"} alt={"icon"} width={140} height={120} style={{marginTop: "17px", marginBottom:"10px"}}></Image>
+          <Image className={styles.productimg} src={"/mastercard.png"} alt={"icon"} width={140} height={120} style={{ marginTop: "17px", marginBottom: "10px" }}></Image>
           <button className={styles.productbutton}>مستر کارت مجازی</button>
         </div>
         <div className={styles.productdiv}>
-          <Image className={styles.productimg} src={"/visa.png"} alt={"icon"} width={140} height={80} style={{marginTop: "37px", marginBottom:"30px"}}></Image>
+          <Image className={styles.productimg} src={"/visa.png"} alt={"icon"} width={140} height={80} style={{ marginTop: "37px", marginBottom: "30px" }}></Image>
           <button className={styles.productbutton}>ویزا کارت مجازی</button>
         </div>
         <div className={styles.productdiv}>
@@ -100,17 +100,47 @@ export default function Home() {
       <div className={styles.introsdiv}>
         <div className={styles.introdiv}>
           <Image className={styles.productimg} src={"/quick.png"} alt={"icon"} width={75} height={75}></Image>
-          <span style={{position:"relative", top:"32.5px"}}>کد محصول بلافاصله پس از خرید تحویل داده میشود.<br></br>از قسمت گزارش خرید تمامی کد های خریداری شده را مشاهده نمایید.</span>
+          <span style={{ position: "relative", top: "32.5px" }}>کد محصول بلافاصله پس از خرید تحویل داده میشود.<br></br>از قسمت گزارش خرید تمامی کد های خریداری شده را مشاهده نمایید.</span>
         </div>
         <div className={styles.introdiv}>
           <Image className={styles.productimg} src={"/precision.png"} alt={"icon"} width={75} height={75}></Image>
-          <span style={{position:"relative", top:"32.5px"}}>خواهشمند است در انتخاب کالا دقت فرمایید; کد تحویل داده شده تعویض و یا مرجوع نمی گردد.</span>
+          <span style={{ position: "relative", top: "32.5px" }}>خواهشمند است در انتخاب کالا دقت فرمایید; کد تحویل داده شده تعویض و یا مرجوع نمی گردد.</span>
         </div>
         <div className={styles.introdiv}>
           <Image className={styles.productimg} src={"/info.png"} alt={"icon"} width={75} height={75}></Image>
-          <span style={{position:"relative", top:"32.5px"}}>جهت تماس با ما میتوانید روش های متفاوتی استفاده کنید.</span>
+          <span style={{ position: "relative", top: "32.5px" }}>جهت تماس با ما میتوانید روش های متفاوتی استفاده کنید.</span>
         </div>
       </div>
+      <div className={styles.contactdiv}>
+        <div className={styles.contactinnerdiv}>
+          <div className={styles.contactinnerinnerdiv}>
+            <Image className={styles.contactinnerimg} src={"/phone-call.png"} alt={"icon"} width={45} height={45}></Image>
+            <div className={styles.contactinnerspandiv}>
+              <span className={styles.contactinnerbigspan}>شماره تماس</span>
+              <br></br>
+              <span className={styles.contactinnersmallspan}>09031974483</span>
+            </div>
+          </div>
+          <div className={styles.contactinnerinnerdiv}>
+            <Image className={styles.contactinnerimg} src={"/email.png"} alt={"icon"} width={45} height={45}></Image>
+            <div className={styles.contactinnerspandiv}>
+              <span className={styles.contactinnerbigspan}>پست الکترونیکی</span>
+              <br></br>
+              <span className={styles.contactinnersmallspan}>sobhanolyayi@gmail.com</span>
+            </div>
+          </div>
+          <div className={styles.contactinnerinnerdiv}>
+            <Image className={styles.contactinnerimg} src={"/location.png"} alt={"icon"} width={45} height={45}></Image>
+            <div className={styles.contactinnerspandiv}>
+              <span className={styles.contactinnerbigspan}>آدرس</span>
+              <br></br>
+              <span className={styles.contactinnersmallspan}>بلوار احمدآباد - احمدآباد 32 - پلاک 54</span>
+            </div>
+          </div>
+        </div>
+        <Image className={styles.mapdiv} src={"/googlemap.png"} alt={"map"} width={500} height={300}></Image>
+      </div>
+      <p>طراح وب سایت : سید محمد سبحان اولیائی</p>
     </main>
   );
 }
